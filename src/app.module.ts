1d180a4dda90b282db24a49683fdea288325bb64
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-11-07 15:38:44 +1100

    adds jobs resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TempsModule } from './temps/temps.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [TempsModule],
+  imports: [TempsModule, JobsModule],
   controllers: [AppController],
   providers: [AppService],
 })
